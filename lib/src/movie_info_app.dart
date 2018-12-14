6f2e763737bb1fae2b83eef83746d7c9edf46269
Ovidio R. Calvet <calveto@gmail.com>
2018-12-14 16:47:41 -0500

    Remove default app

---
+++ lib/src/movie_info_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieInfoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        child: Center(
+          child: Text('All gone...'),
+        ),
+      ),
+    );
+  }
+}
