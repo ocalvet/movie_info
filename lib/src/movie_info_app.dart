8073bd9810c8b1a98f9164b063af6e963c247571
Ovidio R. Calvet <calveto@gmail.com>
2018-12-15 16:59:37 -0500

    Add routes

--- lib/src/movie_info_app.dart
+++ lib/src/movie_info_app.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:movie_info/src/home_page.dart';
+import 'package:movie_info/src/movies_page.dart';
 
 class MovieInfoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        child: Center(
-          child: Text('All gone...'),
-        ),
-      ),
+      theme: ThemeData.dark(),
+      title: 'MovieInfo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/movies': (context) => MoviesPage(),
+      },
     );
   }
 }
---
+++ lib/src/movies_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MovieInfo'),
+      ),
+      body: Center(
+        child: Text('Movies Page'),
+      ),
+    );
+  }
+}
