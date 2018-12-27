1c1a761b9214b04167c1d03be4c67b043d929ea8
Ovidio Calvet <calveto@gmail.com>
2018-12-27 10:44:45 -0500

    Create UpcomingPage

---
+++ lib/src/upcoming_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:movie_info/src/bloc_provider.dart';
+import 'package:movie_info/src/movie_model.dart';
+import 'package:movie_info/src/movie_widget.dart';
+import 'package:movie_info/src/movies_bloc.dart';
+
+class UpcomingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MoviesBloc bloc = BlocProvider.of<MoviesBloc>(context);
+    bloc.getCurrentlyPlayingMovies();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upcoming'),
+      ),
+      body: StreamBuilder(
+        stream: bloc.movies$,
+        builder: (BuildContext context, AsyncSnapshot<List<Movie>> snapshot) {
+          if (snapshot.hasData) {
+            return _buildMovieList(snapshot.data);
+          }
+          return _loading();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          bloc.getCurrentlyPlayingMovies();
+        },
+      ),
+    );
+  }
+
+  Widget _loading() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0),
+          ),
+          Text('Loading Movies')
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMovieList(List<Movie> movieList) {
+    return Center(
+        child: ListView.builder(
+      itemCount: movieList.length,
+      itemBuilder: (BuildContext context, int index) {
+        Movie movie = movieList[index];
+        return MovieWidget(movie: movie);
+      },
+    ));
+  }
+}
