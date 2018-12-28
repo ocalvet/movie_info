68323cb5c63535db11d053278670b15412beb077
Ovidio Calvet <calveto@gmail.com>
2018-12-28 09:24:51 -0500

    Show altitude in the map

--- lib/src/map_page.dart
+++ lib/src/map_page.dart
@@ -26,25 +26,42 @@ class MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
+    double altitude = currentLocation["altitude"] * 3.28084;
     return Scaffold(
         appBar: AppBar(
           title: Text('Map'),
         ),
         body: currentLocation.length > 0
             ? Container(
-                child: GoogleMap(
-                  onMapCreated: (GoogleMapController controller) async {
-                    mapController = controller;
-                  },
-                  options: GoogleMapOptions(
-                    myLocationEnabled: true,
-                    cameraPosition: CameraPosition(
-                        target: LatLng(
-                          currentLocation["latitude"],
-                          currentLocation["longitude"],
+                child: Stack(
+                  children: <Widget>[
+                    GoogleMap(
+                      onMapCreated: (GoogleMapController controller) async {
+                        mapController = controller;
+                      },
+                      options: GoogleMapOptions(
+                        myLocationEnabled: true,
+                        cameraPosition: CameraPosition(
+                            target: LatLng(
+                              currentLocation["latitude"],
+                              currentLocation["longitude"],
+                            ),
+                            zoom: 12.0),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      bottom: 20,
+                      child: Container(
+                        color: Colors.black54,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                              'Current altitude ${altitude.toStringAsFixed(2)} feet'),
                         ),
-                        zoom: 12.0),
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               )
             : Container(
