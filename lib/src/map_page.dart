e8737c85a1dccc03586a22e36118b4e7fb9cf951
Ovidio Calvet <calveto@gmail.com>
2018-12-28 08:54:28 -0500

    Move map to current location

--- lib/src/map_page.dart
+++ lib/src/map_page.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  MapPageState createState() {
+    return new MapPageState();
+  }
+}
+
+class MapPageState extends State<MapPage> {
+  GoogleMapController mapController;
+  Map<String, double> currentLocation;
+  Location location;
+
+  @override
+  void initState() {
+    currentLocation = <String, double>{};
+    location = Location();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +29,20 @@ class MapPage extends StatelessWidget {
       ),
       body: Container(
         child: GoogleMap(
-          onMapCreated: (GoogleMapController controller) {},
+          onMapCreated: (GoogleMapController controller) async {
+            mapController = controller;
+            try {
+              currentLocation = await location.getLocation();
+              controller.animateCamera(
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                      target: LatLng(currentLocation["latitude"], currentLocation["longitude"]), zoom: 12.0),
+                ),
+              );
+            } catch (Exception) {
+              currentLocation = null;
+            }
+          },
           options: GoogleMapOptions(
             myLocationEnabled: true,
           ),
