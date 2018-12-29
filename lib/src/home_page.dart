c6013f9f9540c85e6ab61149cc30a32137b554d9
Ovidio Calvet <calveto@gmail.com>
2018-12-29 10:22:42 -0500

    Add a drawer to the app

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -5,25 +5,68 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MovInfo'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.map),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return MapPage();
-                    },
-                  ),
-                );
+      appBar: AppBar(
+        title: Text('MovInfo'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.map),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return MapPage();
+                  },
+                ),
+              );
+            },
+          )
+        ],
+      ),
+      body: _buildBody(context),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'MovInfo',
+                style: TextStyle(
+                  color: Colors.black87,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.amberAccent,
+              ),
+            ),
+            ListTile(
+              title: Text('Upcoming movies'),
+              subtitle: Text('movies will be playing in the theaters'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/upcoming');
               },
-            )
+            ),
+            ListTile(
+              title: Text('Playing now'),
+              subtitle: Text('movies are playing in the theaters now'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/movies');
+              },
+            ),
+            ListTile(
+              title: Text('Trending'),
+              subtitle: Text('movies that people are currently reviewing'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/trending');
+              },
+            ),
           ],
         ),
-        body: _buildBody(context));
+      ),
+    );
   }
 
   Widget _buildBody(BuildContext context) {
