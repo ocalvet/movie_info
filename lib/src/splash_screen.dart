255339970428321e7f66e46c48518a99933608f2
Ovidio Calvet <calveto@gmail.com>
2019-01-07 10:08:11 -0500

    Add better styles to the splash screen

--- lib/src/splash_screen.dart
+++ lib/src/splash_screen.dart
@@ -15,16 +15,31 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.pushNamed(context, '/');
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         alignment: Alignment.topLeft,
+        fit: StackFit.expand,
         children: [
           Container(
             decoration: BoxDecoration(
               color: Colors.red,
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to the Movie Information App',
+                style: Theme.of(context).textTheme.display1,
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16),),
+              CircularProgressIndicator(),
+            ],
           ),
         ],
       ),
